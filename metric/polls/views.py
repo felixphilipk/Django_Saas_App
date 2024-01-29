bb5d1bdcd230e51e3ad57dc3c84eacec587e9df3
felix <felixphilipk@gmail.com>
2024-01-29 11:36:12 +0530

    views

--- metric/polls/views.py
+++ metric/polls/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+
 
 # Create your views here.
 
+
+def detail(request,question_id):
+      return HttpResponse("Your'e looking at question %s." % question_id)
+
+def results(request,question_id):
+     response = "Your'e looking at the results of question %s."
+     return HttpResponse(response %question_id)
+
+def vote(request,question_id):
+     return HttpResponse("Your'e voting on question %s" % question_id)
+
 def index(request):
-    return HttpResponse("The City of the Dead")
+     latest_questions_list = Question.objects.order_by("-pub_date")[:5]
+     output = ",".join([q.question_text for q in latest_questions_list])
+     return HttpResponse(output)
+
